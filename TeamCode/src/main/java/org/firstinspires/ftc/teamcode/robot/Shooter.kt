3fb856a9ccabf42bf290632e515965889005755e
Emi <emi.serban@gmail.com>
2026-06-20 23:53:08 +0300

    Turret Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.kt
@@ -2,15 +2,27 @@ package org.firstinspires.ftc.teamcode.robot
 
 import com.acmerobotics.dashboard.config.Config
 import com.pedropathing.ftc.localization.Encoder
+import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.Servo
 import com.qualcomm.robotcore.hardware.VoltageSensor
 import org.firstinspires.ftc.teamcode.library.controller.PIDController
 
 class Shooter(
-
+    val motorTop : DcMotorEx,
+    val motorBottom : DcMotorEx,
+    val servo1 : Servo,
+    val servo2 : Servo
 ) {
     @Config
     data object ShooterConfig {
 
     }
+    var motorPower
+        get() = motorTop.power
+        set(value) {
+            motorTop.power = value
+            motorBottom.power = value
+        }
+
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systemsTests/TurretTest.kt
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.systemsTests
+
+import com.bylazar.configurables.annotations.Configurable
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.Servo
+
+@TeleOp
+class TurretTest : LinearOpMode() {
+    @Configurable
+    object OuttakeConfig {
+        @JvmField var motorPower = 0.5
+        @JvmField var turretStep = 0.01
+    }
+
+    override fun runOpMode() {
+        val motorTop = hardwareMap.get(DcMotorEx::class.java, "motorShooterTop")
+        val motorBottom = hardwareMap.get(DcMotorEx::class.java, "motorShooterBottom")
+
+        val servo1 = hardwareMap.get(Servo::class.java, "turretServo1")
+        val servo2 = hardwareMap.get(Servo::class.java, "turretServo2")
+
+        servo1.position = 0.0
+        servo2.position = 0.0
+
+        waitForStart()
+
+        while (opModeIsActive()) {
+            motorTop.power = OuttakeConfig.motorPower
+            motorBottom.power = OuttakeConfig.motorPower
+
+            if (gamepad1.right_bumper) {
+                servo1.position = (servo1.position + OuttakeConfig.turretStep).coerceIn(0.0, 1.0)
+                servo2.position = (servo2.position + OuttakeConfig.turretStep).coerceIn(0.0, 1.0)
+            }
+
+            if (gamepad1.left_bumper) {
+                servo1.position = (servo1.position - OuttakeConfig.turretStep).coerceIn(0.0, 1.0)
+                servo2.position = (servo2.position - OuttakeConfig.turretStep).coerceIn(0.0, 1.0)
+            }
+        }
+    }
+}
